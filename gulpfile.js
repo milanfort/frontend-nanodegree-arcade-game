6019f7ef5c070ba846fc5dfb5466dd74636701f4
Milan Fort <milan.fort@gmail.com>
2015-10-22 01:09:26 +0200

    Create gulp css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,43 @@
 var project = require('./package.json');
+var del = require('del');
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var jslint = require('gulp-jslint');
+var minify = require('gulp-minify-css');
+var prefix = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
 
-gulp.task('js', function() {
-    return gulp.src('app/js/**/*.js')
-        .pipe(concat('main.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'));
+gulp.task('clean', function () {
+    del([
+        'dist'
+    ]);
 });
 
-gulp.task('default', ['js'], function() {
+gulp.task('css', function () {
+    return gulp.src('app/css/main.css')
+        .pipe(minify())
+        .pipe(rename('main.min.css'))
+        .pipe(prefix({
+            browsers: ['last 2 versions', '> 2%'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist/css/'))
+});
+
+gulp.task('js', function () {
+    return gulp.src('app/js/**/*.js')
+        //.pipe(jslint())
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'));
+});
+
+gulp.task('lint', function () {
+    return gulp.src('app/js/app.js')
+        .pipe(jslint())
+});
+
+gulp.task('default', ['css', 'js'], function () {
     console.log('Building %s version %s', project.name, project.version);
 });
