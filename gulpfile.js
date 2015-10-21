efb2d7e48dec769f7112e64fb8389dea2d20ec06
Milan Fort <milan.fort@gmail.com>
2015-10-21 19:55:50 +0200

    Create gulp js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var project = require('./package.json');
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
-gulp.task('default', function() {
+gulp.task('js', function() {
+    return gulp.src('app/js/**/*.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['js'], function() {
     console.log('Building %s version %s', project.name, project.version);
 });
